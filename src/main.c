2da430977ecbfbc580a117ff41d09e731117a4bb
DjoykeAbyah <crudendjoyke@gmail.com>
2024-03-12 19:44:40 +0100

    playing around with windows and Makefile

--- src/main.c
+++ src/main.c
@@ -6,40 +6,52 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/07 18:08:39 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/03/07 18:48:44 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/03/12 19:41:19 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "game_struct.h"
 #include <MLX42/MLX42.h>
 
-#define WIDTH 512
+
+#define WIDTH 1024
 #define HEIGHT 512
 
 static mlx_image_t* image;
 
-// -----------------------------------------------------------------------------
+float py = 300;
+float px = 300;
+int mapX = 8;
+int mapY = 8;
+int mapS = 64;
+int map[] =
+{
+	1,1,1,1,1,1,1,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,0,0,0,1,0,1,
+	1,0,0,0,0,1,0,1,
+	1,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1,
+};
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
     return (r << 24 | g << 16 | b << 8 | a);
 }
 
-void ft_randomize(void* param)
+void ft_player(void* param)
 {
 	(void)param;
 	for (uint32_t i = 0; i < image->width; ++i)
 	{
 		for (uint32_t y = 0; y < image->height; ++y)
 		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
+			uint32_t color = ft_pixel(255, 255, 0, 255);
 			mlx_put_pixel(image, i, y, color);
 		}
 	}
@@ -61,6 +73,34 @@ void ft_hook(void* param)
 		image->instances[0].x += 5;
 }
 
+void draw_map_2d(void *param)
+{
+	(void)param;
+	int x;
+	int y;
+	int xo;
+	int yo;
+	uint32_t color;
+
+	y = 0;
+	while (y < mapY)
+	{
+		y++;
+		x = 0;
+		while (x < mapX)
+		{
+			x++;
+			if (map[y * mapX + x] == 1)
+				color = ft_pixel(0, 0, 0, 0);
+			else
+				color = ft_pixel(255, 255, 255, 255);
+			xo = x *mapS;
+			yo = y *mapS;
+			mlx_put_pixel(image, xo, yo, color);
+		}
+	}
+}
+
 // -----------------------------------------------------------------------------
 
 int32_t main(void)
@@ -73,35 +113,23 @@ int32_t main(void)
 		printf("%s", mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
+	if (!(image = mlx_new_image(mlx, 8, 8)))//draw player
 	{
 		mlx_close_window(mlx);
 		printf("%s", mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	if (mlx_image_to_window(mlx, image, px, py) == -1)
 	{
 		mlx_close_window(mlx);
 		printf("%s", mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
+	// mlx_loop_hook(mlx, draw_map_2d, mlx);
+	mlx_loop_hook(mlx, ft_player, mlx);
 	mlx_loop_hook(mlx, ft_hook, mlx);
 
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
-
-// int32_t main(int argc, char *argv[])
-// {
-// 	//data_t data;
-	
-// 	if (argc != 2)
-// 		return (0);
-// 	//parse_input(argv[1], &data);
-// 	//do_math(&data);
-// 	//start_mlx(&data);
-// 	//clear(&data);
-// }
